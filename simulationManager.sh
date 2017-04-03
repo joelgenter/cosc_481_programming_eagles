e8f66bef024843b9c1144502bff89b3675e1c618
Joel Genter <joel.genter@ge.com>
2017-04-03 17:47:18 -0400

    implement db polling every minute

--- simulationManager.sh
+++ simulationManager.sh
@@ -1,5 +1,6 @@
-# COUNTER=
-# while 
-echo "hello";
-
-root@localhost: >5lJDsOeALhe
+#!/bin/bash
+SIMULATION_IS_QUEUED=0;
+while [$SIMULATION_IS_QUEUED -eq 0]
+do
+    myvar=$(mysql testdb -u root -ptestpass -se "SELECT EXISTS(SELECT 1 FROM users WHERE firstName = 'Joel')")
+echo $myvar;
