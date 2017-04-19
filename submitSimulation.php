179eedbc9be7a5828af053dda781927c70a6ec8d
jginnard <jginnard@emich.edu>
2017-04-18 20:13:45 -0400

    uploading files with php.

--- submitSimulation.php
+++ submitSimulation.php
@@ -1,12 +1,43 @@
 <?php
 require 'db_connection.php';        //$conn (mysqli connection) is now available
 
+//File Upload
+$target_dir = "uploads/";
+$target_file = $target_dir . basename($_FILES["pdbFile"]["name"]);
+$uploadOk = 1;
+$fileType = pathinfo($target_file,PATHINFO_EXTENSION);
+
+// Check if file already exists
+if (file_exists($target_file)) {
+    echo "Sorry, file already exists.";
+    $uploadOk = 0;
+}
+// Check file size
+if ($_FILES["pdbFile"]["size"] > 50000000) { //50MB limit
+    echo "Sorry, your file is too large.";
+    $uploadOk = 0;
+}
+// Allow certain file formats
+if($fileType != "pdb") {
+    echo "Sorry, only PDB files are allowed.";
+    $uploadOk = 0;
+}
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+    echo "Sorry, your file was not uploaded.";
+// if everything is ok, try to upload file
+} else {
+    if (move_uploaded_file($_FILES["pdbFile"]["tmp_name"], $target_file)) {
+        echo "The file ". basename( $_FILES["pdbFile"]["name"]). " has been uploaded.";
+    } else {
+        echo "Sorry, there was an error uploading your file.";
+    }
+}
+
+
 //Form data
 $pdbFileName = filter_var ($_POST["pdbFileName"], FILTER_SANITIZE_STRING);
-$pdbFile;
-if (pathinfo($pdbFileName)['extension'] == "pdb" && filesize ( $pdbFile ) <= 10000000){
-$pdbFile =  file_get_contents ($_POST["pdbFile"]);
-}
+$pdbFile = $target_file;
 $mutationList = filter_var ($_POST["mutationList"], FILTER_SANITIZE_STRING);
 $username = filter_var ($_POST["username"], FILTER_SANITIZE_STRING);
 $simulationName = filter_var ($_POST["simulationName"], FILTER_SANITIZE_STRING);
@@ -44,6 +75,7 @@ if (mysqli_multi_query($conn, $query)) {
     echo "Error: " . $query . "<br>" . mysqli_error($conn);
 }
 
+
 mysqli_close($conn);
-header("Location: queue.php");
-die();
+// header("Location: queue.php");
+// die();
