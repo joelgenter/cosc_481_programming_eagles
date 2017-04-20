b925abb59a24374318568ccfe9094eb6d6268e6f
ikane12 <ikane@emich.edu>
2017-04-20 08:01:16 -0800

    Attempt again
    please work.

--- resultPage.php
+++ resultPage.php
@@ -91,7 +91,7 @@ $id = $_GET['id'];
 	  </div><!-- /.container-fluid -->
 	</nav>
 	<!-- Page Content -->
-	<div class="container-fluid container-body"style="padding-top: 20px;">
+	<div id ='body' class="container-fluid container-body"style="padding-top: 20px;">
 		<div class="row">
 				<div class="col-lg-12">
 					<div class="title">
@@ -115,8 +115,6 @@ $id = $_GET['id'];
 					</div>
 				</div>
 			</legend>
-			
-		<div>
 		<div>
 		<div class = 'row row-eq-height'>
 			<div class = 'col-sm-4'>
@@ -140,7 +138,7 @@ $id = $_GET['id'];
 		</div>
 		</br>
 		</br>
-			<canvas id="myChart" width="1027px" height="500px"></canvas>
+		<canvas id="myChart" height = '100px'></canvas>
 		</div>
 		<p>
 			<legend>Description</legend>
@@ -157,15 +155,14 @@ $id = $_GET['id'];
 	</div>
 </div>
 <script>
-   generateResults(<?php echo "'".$id."'" ?>,'freeEnergy')
-   
-   function changeGraph(stuff){
+      function changeGraph(stuff){
 	   for(var child of $('[name="dropdown"]')){
 		   $(child).attr('class','');
 	   }
 	   $(stuff).attr('class','active');
 	   generateResults(<?php echo "'".$id."'" ?>,$(stuff).attr('id'))
    }
+   generateResults(<?php echo "'".$id."'" ?>,'freeEnergy')
 </script>
 </div>
 	<div class="container-fluid">
