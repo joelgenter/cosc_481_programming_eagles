2642a8e0c9db9e7bbe42b6a2185fa287eafbeafb
ikane12 <ikane@emich.edu>
2017-04-21 19:36:32 -0800

    Trying my hand at caching fix
    This might work?

---
+++ admin.js
@@ -0,0 +1,122 @@
+var admin = 'admin';
+var standard = 'standard';
+var pending = 'pending';
+var userCount = 0;
+
+//------------------------------------------------------------------------------
+// Removes the user that the selected is next to from Users in ProteinSim, then
+// removes the user from the userList HTML object.
+
+    function remUser(idLi){
+			// var appCache = window.applicationCache;
+			// appCache.update(); //this will attempt to update the users cache and changes the application cache status to 'UPDATEREADY'.
+			// if (appCache.status == window.applicationCache.UPDATEREADY) {
+			// 	appCache.swapCache(); //replaces the old cache with the new one.
+			// }
+      // Get username
+      var username = nodeToString(document.getElementById('username'+idLi));
+					username = username.replace('<div class="col-lg-6" id="username'+idLi+'">','');
+					username = username.replace('</div>','');
+					console.log(username);
+      // Remove from database
+			$.ajax({
+				data: 'username=' + username,
+				url: 'removeUser.php',
+				method: 'POST',
+				// If successful, standard message
+				success: function(message) {
+					console.log(message);
+				}
+			});
+      // Remove user from the userList
+      document.getElementById('row'+idLi).remove();
+    }
+
+//------------------------------------------------------------------------------
+// Updates the user type with option selected in dropdown menu, then change
+// the HTML code to reflect said change.
+
+		function updateType(dropType,idLi){
+			// var appCache = window.applicationCache;
+			// appCache.update(); //this will attempt to update the users cache and changes the application cache status to 'UPDATEREADY'.
+			// if (appCache.status == window.applicationCache.UPDATEREADY) {
+			//   appCache.swapCache(); //replaces the old cache with the new one.
+			// }
+			// Get username
+			var username = nodeToString(document.getElementById('username'+idLi));
+					username = username.replace('<div class="col-lg-6" id="username'+idLi+'">','');
+					username = username.replace('</div>','');
+					console.log(username);
+			// Update the database
+			$.ajax({
+				data: 'username=' + username + '&userType=' + dropType,
+				url: 'updateUsers.php',
+				method: 'POST',
+				// If successful, standard message
+				success: function(message) {
+					console.log(message);
+				}
+			});
+			// Update HTML to show new dropType value for said username
+			document.getElementById('typeButton'+idLi).innerHTML =
+						dropType +
+						'<span class="caret"></span>';
+		}
+
+//------------------------------------------------------------------------------
+// Fuction is used to convert node into string.
+
+		function nodeToString ( node ) {
+			var tmpNode = document.createElement( "div" );
+			tmpNode.appendChild( node.cloneNode( true ) );
+			var str = tmpNode.innerHTML;
+			tmpNode = node = null; // prevent memory leaks in IE
+			return str;
+		}
+
+
+function updateHtml(userData, email){
+	for(var user of userData){
+		console.log((user.email+"  "+email))
+		{$(usersList).append(
+					'<div id="row'+userCount+'" class="list-group-item list-group-item-action userList">' +
+						'<div class = "row">' +
+							'<div class = "col-lg-6" id = "username' + userCount + '">' +
+								user.username +
+							'</div>' +
+								'<div class = "col-lg-4">' +
+									'<div class="dropdown">'+
+										'<button style="width: 8em;" '+
+										'class="'+((email== user.email)? 'disabled': '')+' btn btn-secondary dropdown-toggle" '+
+										'id="typeButton'+userCount+'" type="button" data-toggle="dropdown">' + user.type +
+										'<span class="caret"></span></button>' +
+										'<ul id="typelist'+userCount+'" class="dropdown-menu">' +
+											'<li><a href="#" id="A'+userCount+'" onclick="updateType(admin,'+userCount+')">admin</a></li>'+
+											'<li><a href="#" id="S'+userCount+'" onclick="updateType(standard,'+userCount+')">standard</a></li>'+
+											'<li><a href="#" id="P'+userCount+'" onclick="updateType(pending,'+userCount+')">pending</a></li>'+
+											'</ul>' +
+									'</div>' +
+								'</div>' +
+							'<div class = "col-lg-2">' +
+								'<i id="remove'+userCount+'" '+
+								'onclick="'+((email== user.email)? 'cantDelete()': 'remUser('+userCount+')')+'" '+
+								'class="'+((email== user.email)? 'disabled': '')+' fa fa-remove"></i>' +
+							'</div>' +
+						'</div>' +
+					'</div>');
+
+				
+				userCount++;
+		}
+	}
+}
+
+function cantDelete(){
+	alert("you can't delete yourself!")
+}
+
+function generateUsers(email){
+	//fixes html in tree
+	$.ajax({url: 'getUsers.php', method: 'POST', 
+			success: function(userData){updateHtml(JSON.parse(userData),email);}});
+}
--- admin.php
+++ admin.php
@@ -69,6 +69,7 @@ $email = $user->getEmail($oauth_uid);
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
 <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+<script src="admin.js"></script>
 <link rel="stylesheet" href="custom.css">
 <style>
 	.form-control-inline {
@@ -170,119 +171,8 @@ $email = $user->getEmail($oauth_uid);
 </div>
 
 <script>
-	var admin = 'admin';
-	var standard = 'standard';
-	var pending = 'pending';
-	var userCount = 0;
+generateUsers(<?php echo("'".$email['email']."'"); ?>);	
 
-	var $usersList = $("#usersList");
-
-	$.get('getUsers.php')
-		.done(function(users) {
-            users = JSON.parse(users);
-			for (var i = 0; i < users.length; i++) {
-				if(users[i].email != <?php echo("'".$email['email']."'"); ?>){
-				var html =
-					'<div id="row'+userCount+'" class="list-group-item list-group-item-action userList">' +
-						'<div class = "row">' +
-							'<div class = "col-lg-6" id = "username' + userCount + '">' +
-								users[i].username +
-							'</div>' +
-								'<div class = "col-lg-4">' +
-									'<div class="dropdown">'+
-										'<button style="width: 8em;" class="btn btn-secondary dropdown-toggle" id="typeButton'+userCount+'" type="button" data-toggle="dropdown">' + users[i].type +
-										'<span class="caret"></span></button>' +
-										'<ul id="typelist'+userCount+'" class="dropdown-menu">' +
-											'<li><a href="#" id="A'+userCount+'" onclick="updateType(admin,'+userCount+')">admin</a></li>'+
-											'<li><a href="#" id="S'+userCount+'" onclick="updateType(standard,'+userCount+')">standard</a></li>'+
-											'<li><a href="#" id="P'+userCount+'" onclick="updateType(pending,'+userCount+')">pending</a></li>'+
-											'</ul>' +
-									'</div>' +
-								'</div>' +
-							'<div class = "col-lg-2">' +
-								'<i id="remove'+userCount+'" onclick="remUser('+userCount+')" class="fa fa-remove"></i>' +
-							'</div>' +
-						'</div>' +
-					'</div>';
-
-				$usersList.append(html);
-				userCount++;
-				}
-			}
-		});
-
-//------------------------------------------------------------------------------
-// Removes the user that the selected is next to from Users in ProteinSim, then
-// removes the user from the userList HTML object.
-
-    function remUser(idLi){
-			// var appCache = window.applicationCache;
-			// appCache.update(); //this will attempt to update the users cache and changes the application cache status to 'UPDATEREADY'.
-			// if (appCache.status == window.applicationCache.UPDATEREADY) {
-			// 	appCache.swapCache(); //replaces the old cache with the new one.
-			// }
-      // Get username
-      var username = nodeToString(document.getElementById('username'+idLi));
-					username = username.replace('<div class="col-lg-6" id="username'+idLi+'">','');
-					username = username.replace('</div>','');
-					console.log(username);
-      // Remove from database
-			$.ajax({
-				data: 'username=' + username,
-				url: 'removeUser.php',
-				method: 'POST',
-				// If successful, standard message
-				success: function(message) {
-					console.log(message);
-				}
-			});
-      // Remove user from the userList
-      document.getElementById('row'+idLi).remove();
-    }
-
-//------------------------------------------------------------------------------
-// Updates the user type with option selected in dropdown menu, then change
-// the HTML code to reflect said change.
-
-		function updateType(dropType,idLi){
-			// var appCache = window.applicationCache;
-			// appCache.update(); //this will attempt to update the users cache and changes the application cache status to 'UPDATEREADY'.
-			// if (appCache.status == window.applicationCache.UPDATEREADY) {
-			//   appCache.swapCache(); //replaces the old cache with the new one.
-			// }
-			// Get username
-			var username = nodeToString(document.getElementById('username'+idLi));
-					username = username.replace('<div class="col-lg-6" id="username'+idLi+'">','');
-					username = username.replace('</div>','');
-					console.log(username);
-			// Update the database
-			$.ajax({
-				data: 'username=' + username + '&userType=' + dropType,
-				url: 'updateUsers.php',
-				method: 'POST',
-				// If successful, standard message
-				success: function(message) {
-					console.log(message);
-				}
-			});
-			// Update HTML to show new dropType value for said username
-			document.getElementById('typeButton'+idLi).innerHTML =
-						dropType +
-						'<span class="caret"></span>';
-		}
-
-//------------------------------------------------------------------------------
-// Fuction is used to convert node into string.
-
-		function nodeToString ( node ) {
-			var tmpNode = document.createElement( "div" );
-			tmpNode.appendChild( node.cloneNode( true ) );
-			var str = tmpNode.innerHTML;
-			tmpNode = node = null; // prevent memory leaks in IE
-			return str;
-		}
-
-//------------------------------------------------------------------------------
 </script>
 </body>
 </html>
